74d613d55854479440aedb5f488c09c6c0b72f4c
Joseph Wright <rjosephwright@gmail.com>
2024-11-09 14:50:56 -0500

    Add missing file

---
+++ src/writable.rs
@@ -0,0 +1,46 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
+
+use anyhow::{anyhow, Result};
+use rustix::fs::{chown, Gid, Mode, OpenOptionsExt, Uid};
+
+use crate::fs::{mkdir_p_own, JoinRelative};
+
+pub trait Writable
+where
+    Self: Read,
+{
+    fn name(&self) -> &str;
+    fn is_secret(&self) -> bool;
+
+    fn write(&mut self, dest: &Path, user_id: u32, group_id: u32) -> Result<()> {
+        let mode_dir = Mode::from(if self.is_secret() { 0o700 } else { 0o755 });
+        let mode_file = Mode::from(if self.is_secret() { 0o600 } else { 0o644 });
+        let name = self.name();
+        let final_dest = if name.is_empty() {
+            dest.to_path_buf()
+        } else {
+            dest.join_relative(name)
+        };
+        let dest_dir = final_dest.parent().ok_or(anyhow!("no parent directory"))?;
+
+        let (uid, gid) = unsafe { (Uid::from_raw(user_id), Gid::from_raw(group_id)) };
+        mkdir_p_own(dest_dir, mode_dir, Some(uid), Some(gid))?;
+
+        let mut f = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .mode(mode_file.as_raw_mode())
+            .open(&final_dest)?;
+
+        io::copy(self, &mut f)?;
+
+        chown(final_dest, Some(uid), Some(gid))?;
+
+        Ok(())
+    }
+}
